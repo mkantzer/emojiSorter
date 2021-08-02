366fd992fd9003ecbd67714bb31f476974217920
Mike Kantzer <mike.kantzer+git@drizly.com>
2021-08-02 09:56:43 -0400

    Setting up SOME stuff?

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,135 +2,13 @@ package db
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"strings"
-	"time"
 
 	"github.com/dstotijn/go-notion"
 	"github.com/mkantzer/emojiSorter/internal/core"
-	"go.uber.org/zap"
 )
 
-type Dependencies struct {
-	Logger *zap.Logger
-}
-
-type NotionDB struct {
-	Deps *Dependencies
-	DbID string
-
-	client *notion.Client
-}
-
-func NewDatabase(deps *Dependencies, dbID string, apiKey string) (NotionDB, error) {
-	emojiCodeMap = emojiCode()
-	return NotionDB{
-		Deps:   deps,
-		DbID:   dbID,
-		client: notion.NewClient(apiKey),
-	}, nil
-}
-
-// queryDatabase wraps the *notion.client.QueryDatabase call for our db struct.
-// It adds some useful debugger logging, (metrics handling and gathering), and (something else).
-// It does NOT modify the DatabaseQueryResponse or error, and returns them directly.
-func (n NotionDB) queryDatabase(ctx context.Context, query *notion.DatabaseQuery) (notion.DatabaseQueryResponse, error) {
-	start := time.Now()
-	n.Deps.Logger.Sugar().Debugw("notion query start",
-		"query", query,
-	)
-	response, err := n.client.QueryDatabase(ctx, n.DbID, query)
-	if err != nil {
-		n.Deps.Logger.Sugar().Error("query failed",
-			"duration", time.Since(start),
-			"error", err.Error(),
-		)
-		return notion.DatabaseQueryResponse{}, err
-	}
-
-	n.Deps.Logger.Sugar().Debugw("query suceeded",
-		"duration", time.Since(start),
-		"numResults", len(response.Results),
-	)
-	return response, nil
-}
-
-func (n NotionDB) extractEmojiFromPage(ctx context.Context, emojiPage notion.Page) (core.Emoji, error) {
-	emojiMap := emojiPage.Properties.(notion.DatabasePageProperties)
-
-	nameProperty, ok := emojiMap["Name"]
-	if !ok {
-		return core.Emoji{}, errors.New("returned page does not have section 'Name'")
-	}
-	if nameProperty.Type != notion.DBPropTypeTitle {
-		return core.Emoji{}, fmt.Errorf("returned 'Name' property does not have Type %s", notion.DBPropTypeTitle)
-	}
-	nameHolder := nameProperty.Title[0].PlainText
-
-	imageProperty, ok := emojiMap["Image"]
-	if !ok {
-		return core.Emoji{}, errors.New("returned page does not have section 'Image'")
-	}
-	if imageProperty.Type != notion.DBPropTypeFiles {
-		return core.Emoji{}, fmt.Errorf("returned 'Image' property does not have Type %s", notion.DBPropTypeFiles)
-	}
-
-	imageURLHolder := imageProperty.Files[0].Name
-	aliasHolder := ""
-
-	// If the image URL starts with "alias:", we need to go find the ACTUAL image URL
-	if strings.HasPrefix(imageURLHolder, "alias:") {
-		aliasHolder = imageProperty.Files[0].Name
-
-		aliasName := strings.TrimPrefix(imageURLHolder, "alias:")
-		// check if predefined. If so, return unicode code
-		if code, ok := emojiCodeMap[":"+aliasName+":"]; ok {
-			imageURLHolder = code
-
-		} else {
-			aliasedEmoji, err := n.GetEmojiByName(ctx, strings.TrimPrefix(imageURLHolder, "alias:"))
-			if err != nil {
-				return core.Emoji{}, fmt.Errorf(
-					"error retrieving emoji %s aliased by %s: %w",
-					strings.TrimPrefix(imageURLHolder, "alias:"),
-					nameHolder,
-					err,
-				)
-			}
-			imageURLHolder = aliasedEmoji.ImageURL
-		}
-	}
-
-	// Vote Stuff
-	voteHolder := make(map[core.Vote]uint)
-	votes := []core.Vote{
-		core.Keep,
-		core.Delete,
-		core.Transparent,
-		core.Rename,
-		core.Duplicate,
-	}
-
-	for _, v := range votes {
-		votePageProperty, ok := emojiMap[fmt.Sprintf("Votes for %s", v.String())]
-		if votePageProperty.Type == "" {
-			voteHolder[v] = uint(0)
-			continue
-		}
-		if !ok {
-			return core.Emoji{}, fmt.Errorf("returned page does not have section 'Votes for %s'", v.String())
-		}
-		if votePageProperty.Type != notion.DBPropTypeNumber {
-			return core.Emoji{}, fmt.Errorf("returned 'Votes for %s' property does not have Type %s", v.String(), notion.DBPropTypeNumber)
-		}
-		voteHolder[v] = uint(*votePageProperty.Number)
-	}
-
-	return core.Emoji{
-		Name:     nameHolder,
-		ImageURL: imageURLHolder,
-		AliasFor: aliasHolder,
-		Votes:    voteHolder,
-	}, nil
+type notionDB interface {
+	queryDB(ctx context.Context, query *notion.DatabaseQuery) (notion.DatabaseQueryResponse, error)
+	extractEmojiFromPage(ctx context.Context, emojiPage notion.Page) (core.Emoji, error)
+	getNextVote(ctx context.Context) (core.Emoji, error)
 }
